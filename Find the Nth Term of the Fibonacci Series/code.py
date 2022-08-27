13046e2979468d445889c47c84a74a33567ec3ad
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-08-28 01:05:04 +0530

    Create code.py

---
+++ Find the Nth Term of the Fibonacci Series/code.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n == 1:
+    print(0)
+elif n == 2:
+    print(1)
+else:
+    l = [0,1]
+    while n>2:
+        l.append((l[-1]+l[-2]))
+        n-=1
+    print(l[-1])        
