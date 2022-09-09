b8bf4c796ababb1b919feba8c520f9b943d2ce11
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-09-09 21:53:24 +0530

    Update code.py

--- Palindrome number/code.py
+++ Palindrome number/code.py
@@ -2,10 +2,10 @@ n = input()
 c = 0
 for i in range(len(n)):
     if n[i] == n[len(n)-i-1]:
-        c = 1
+        c += 1
     else:
         c = 0
-if c == 1:
+if c == len(n):
     print("Pallindrome")
 else:
     print("Not Pallindrome")
